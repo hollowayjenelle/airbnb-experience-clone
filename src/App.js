6f76068a56f9c064ad4f6f568f8909fc9f9a6116
jholloway <jholloway@griddynamics.com>
2023-01-17 16:11:53 -0500

    change App.js and Card.js to use spread object as props

--- src/App.js
+++ src/App.js
@@ -7,12 +7,8 @@ import dataArr from './data'
 function App() {
   const cards = dataArr.map(obj => {
     return <Card 
-          img = {obj.coverImg}
-          rating = {obj.stats.rating}
-          reviewCount = {obj.stats.reviewCount}
-          country = {obj.location}
-          title = {obj.title}
-          price = {obj.price}
+          key={obj.id}
+          {...obj}
     />
   })
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,20 @@
 export default function Card(props){
+    let badgeText
+    if(props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    }else if (props.location === 'Online'){
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card-container">
-            <div className="image-container">
-                <img src={`./images/${props.img}`} alt="lady in swimwear smiling"/>
-            </div>
+            {badgeText && <div className="card-badge">{badgeText}</div>}
+            <img className="card-img" src={`./images/${props.coverImg}`} alt="lady in swimwear smiling"/>
             <div className="ratings">
                 <img src="./images/star.png" alt="red star" className="star-img"/>
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) -</span>
-                <span className="gray">{props.country}</span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) -</span>
+                <span className="gray">{props.location}</span>
             </div>
             <p className="experience-description">{props.title}</p>
             <p className="pricing"><strong>From ${props.price}</strong> / person</p>
