27695d0c7216055aad33ba4b65b9c1cd63e6e05e
jholloway <jholloway@griddynamics.com>
2023-01-17 11:56:06 -0500

    add styling to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,5 +2,18 @@ import star from "../images/star.png"
 import image1 from "../images/image1.png"
 
 export default function Card(){
-    
+    return (
+        <div className="card-container">
+            <div className="image-container">
+                <button className="status">SOLD OUT</button>
+                <img src={image1} alt="lady in swimwear smiling"/>
+            </div>
+            <div className="ratings">
+                <img src={star} alt="red star" className="star-img"/>
+                <span>5.0 &#40;6&#41; - USA</span>
+            </div>
+            <p className="experience-description">Life lessons with Katie Zaferes</p>
+            <p className="pricing"><strong>From $136</strong> / person</p>
+        </div>
+    )
 }
