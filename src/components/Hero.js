df9137d14e51719b291405e027ca2221e9368720
jholloway <jholloway@griddynamics.com>
2023-01-17 14:41:20 -0500

    change alt text in Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ export default function Hero(){
     return(
         <div className="hero-display">
             <div className="grid-container">
-            <img src={grid} alt="photo grid containing different pictures" className="photo-grid"/>
+            <img src={grid} alt="grid of different experiences" className="photo-grid"/>
             </div>
             <h1 className="main-heading">Online Experiences</h1>
             <p className="description">Join unique interactive activities led by <br></br>one-of-a-kind-hosts-all without leaving <br></br> home</p>
