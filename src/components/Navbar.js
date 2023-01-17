4667d5a1f8ef6c6b0030c33c52d6e7e4413b94a1
jholloway <jholloway@griddynamics.com>
2023-01-17 10:17:01 -0500

    create images folder; changes made to Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,9 @@
+import logo from "../images/airbnb-logo.png"
+
 export default function Navbar(){
-    
+    return(
+        <nav>
+            <img src={logo} alt="airbnb logo"/>
+        </nav>
+    )
 }
